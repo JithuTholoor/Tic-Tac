665753b7a87a4c5ff542a753b5a99c139cfd5d0c
JiTHu <jithu.tholoor@gmail.com>
2017-02-03 18:26:58 +0530

    re-arraging test files

--- src/game/__test__/game.test.js
+++ src/game/__test__/game.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { render } from 'enzyme';
-import Game from './game';
+import Game from './../game';
 
 describe('Game component',()=>{
     it('renders without crashing', () => {
